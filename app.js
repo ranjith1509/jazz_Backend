2e94499e9fb563532e40f60396ef730c29bf2dbc
RANJITHMEENA <ranjithkumarproton@gmail.com>
2024-03-10 03:23:02 +0530

    env add

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const adminRegister = require("./router/adminRegister");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = "https://jazzbackend.netlify.app";
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_DB_CONNECTION, {
